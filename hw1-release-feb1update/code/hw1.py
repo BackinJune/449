438e7167386cb2f30c0fc14b5a65544fd29fd324
Bingjun Guo <bingjun3@illinois.edu>
2024-02-06 09:34:07 -0600

    first hw

---
+++ hw1-release-feb1update/code/hw1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import hw1_utils as utils
+
+def linear_gd(X,Y,lrate=0.1,num_iter=1000):
+    # return parameters as numpy array
+    return []
+
+def linear_normal(X,Y):
+    # return parameters as numpy array
+    return []
+
+def plot_linear():
+    # return plot
+    return []
