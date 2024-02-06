fa17c0ca34c5f40ac77bdf9d41d34bc9b40a3022
Bingjun Guo <bingjun@guobingjundeMacBook-Air.local>
2024-02-06 16:37:55 -0600

    code done

--- hw1-release-feb1update/code/hw1.py
+++ hw1-release-feb1update/code/hw1.py
@@ -2,11 +2,13 @@ import numpy as np
 import hw1_utils as utils
 import matplotlib.pyplot as plt
 
-def linear_gd(X,Y,lrate=0.1,num_iter=100):
+def linear_gd(X,Y,lrate=0.001,num_iter=100):
     # return parameters as numpy array
+    # X: N * d; Y: N,
     w = np.zeros([X.shape[1] + 1, 1])
-    X_alt_T = np.append(X, np.ones([X.shape[0], X.shape[1]]), 1)
-    for i in range(1):
+    X_alt_T = np.append(X, np.ones([X.shape[0], 1]), 1)
+    # print(X_alt_T.shape, Y.shape)
+    for i in range(num_iter):
         grad = np.dot(X_alt_T.T, X_alt_T)
         grad = np.dot(grad, w)
         grad -= np.dot(X_alt_T.T, Y.reshape([-1, 1]))
@@ -25,12 +27,12 @@ def linear_normal(X,Y):
 def plot_linear():
     # return plot
     X, Y = utils.load_reg_data()
-    plt.plot(X, Y, 'bo')
-    X_t = np.linspace(0, 4, 100)
-    w = linear_gd(X, Y)
-    Y_t = X_t * w[0] + w[1]
-    plt.plot(X_t, Y_t)
-    plt.show()
-    return []
+    ret_plot = plt.subplot()
+    ret_plot.plot(X, Y, 'bo')
+    w = linear_normal(X, Y)
+    Y_t = np.dot(w[:-1], X.T) + w[-1]
+    ret_plot.plot(X, Y_t)
+    return ret_plot
 
-plot_linear()
+# ret_plot = plot_linear()
+# plt.show(ret_plot)
