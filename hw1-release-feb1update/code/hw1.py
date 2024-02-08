47e4668af0425c4ba56b596f0aedd142c9798252
Bingjun Guo <bingjun3@illinois.edu>
2024-02-08 16:57:58 -0600

    hw1 done

--- hw1-release-feb1update/code/hw1.py
+++ hw1-release-feb1update/code/hw1.py
@@ -6,34 +6,29 @@ def linear_gd(X,Y,lrate=0.1,num_iter=100):
     # return parameters as numpy array
     # X: N * d; Y: N,
     w = np.zeros([X.shape[1] + 1, 1])
-    X_alt_T = np.append(X, np.ones([X.shape[0], 1]), 1)
-    # print(X_alt_T.shape, Y.shape)
+    X = np.c_[np.ones([X.shape[0], 1]), X]
+    Y = Y.reshape([-1, 1])
     for i in range(num_iter):
-        grad = np.dot(X_alt_T.T, X_alt_T)
-        grad = np.dot(grad, w)
-        grad -= np.dot(X_alt_T.T, Y.reshape([-1, 1]))
-        w -= np.log(np.log(np.abs(grad * lrate) + 1) + 1) * 0.01 * np.sign(grad)
-    return w.reshape([1, -1])[0]
+        w -= lrate * (X.T @ X @ w - X.T @ Y) / X.shape[0]
+    print(w.shape)
+    return w
 
 def linear_normal(X,Y):
     # return parameters as numpy array
-    X_alt_T = np.append(X, np.ones([X.shape[0], 1]), 1)
-    Y_alt = Y.reshape([-1, 1])
-    inverse = np.linalg.pinv(np.dot(X_alt_T.T, X_alt_T))
-    w = np.dot(X_alt_T.T, Y_alt)
-    w = np.dot(inverse, w)
-    return w.reshape([1, -1])[0]
+    X = np.c_[np.ones([X.shape[0], 1]), X]
+    Y = Y.reshape([-1, 1])
+    return np.linalg.pinv(X.T @ X) @ X.T @ Y
 
 def plot_linear():
     # return plot
     X, Y = utils.load_reg_data()
-    print(X.shape)
     ret_plot = plt.subplot()
     ret_plot.plot(X, Y, 'bo')
     w = linear_normal(X, Y)
-    Y_t = np.dot(w[:-1], X.T) + w[-1]
+    Y_t = X @ w[1:] + w[0]
+    print(w)
     ret_plot.plot(X, Y_t)
     return ret_plot
 
-ret_plot = plot_linear()
-plt.show()
+# ret_plot = plot_linear()
+# plt.show()
