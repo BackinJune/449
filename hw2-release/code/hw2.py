0c8c694d1c107f376e3022c1e1209a6e22b4b3cb
Bingjun Guo <bingjun3@illinois.edu>
2024-02-20 23:47:11 -0600

    hw2 done

--- hw2-release/code/hw2.py
+++ hw2-release/code/hw2.py
@@ -34,7 +34,25 @@ def svm_solver(x_train, y_train, lr, num_iters,
     You will then need to use alpha.requires_grad_().
     Alternatively, use in-place operations such as clamp_().
     '''
-    pass
+    n = y_train.size()[0]
+    alpha = torch.zeros([n, 1]) # a plain tensor
+    alpha.requires_grad_()
+    optimizer = optim.SGD([alpha], lr=lr)
+    for _ in range(num_iters):
+        optimizer.zero_grad()
+        K = torch.zeros([n, n])
+        for i in range(n):
+            for j in range(n):
+                K[i][j] = kernel(x_train[i], x_train[j])
+        f = 0.5 * (alpha.t() * y_train.view((1, n))) @ K @ (alpha * y_train.view(n, 1)) - torch.ones([1, n]) @ alpha
+        f.backward()
+        optimizer.step()
+        with torch.no_grad():
+            alpha[alpha < 0] = 0
+            alpha -= lr * alpha.grad
+            if c!=None:
+                alpha[alpha > c] = c
+    return alpha.to(dtype=torch.float32).squeeze()
 
 def svm_predictor(alpha, x_train, y_train, x_test,
                   kernel=hw2_utils.poly(degree=1)):
@@ -53,5 +71,72 @@ def svm_predictor(alpha, x_train, y_train, x_test,
     Return:
         A 1d tensor with shape (m,), the outputs of SVM on the test set.
     '''
-    pass
+    n = alpha.size()[0]
+    m = x_test.size()[0]
+    y_test = torch.zeros([m,])
+    for j in range(m):
+        K = torch.zeros((n, 1))
+        for i in range(n):
+            K[i] = kernel(x_test[j], x_train[i])
+            y_test[j] = (alpha.view((n, 1)) * y_train.view((n, 1)) * K).sum()
+    return y_test   
 
+# x, y = hw2_utils.xor_data()
+# print(svm_solver(x, y, 0.1, 100, hw2_utils.poly(2), 0.8))
+# alpha_1 = svm_solver(x, y, 0.1, 10000, hw2_utils.poly(2), 0.8)
+# alpha_2 = svm_solver(x, y, 0.1, 10000, hw2_utils.rbf(1), 0.8)
+# alpha_3 = svm_solver(x, y, 0.1, 10000, hw2_utils.rbf(2), 0.8)
+# alpha_4 = svm_solver(x, y, 0.1, 10000, hw2_utils.rbf(3), 0.8)
+
+# def pred_f1(x_test):
+#     n = alpha_1.size()[0]
+#     m = x_test.size()[0]
+#     kernel = hw2_utils.poly(2)
+#     y_test = torch.from_numpy(np.zeros([m,]))
+#     for j in range(m):
+#         K = torch.zeros((n, 1))
+#         for i in range(n):
+#             K[i] = kernel(x_test[j], x[i])
+#             y_test[j] = (alpha_1.view((n, 1)) * y.view((n, 1)) * K).sum()
+#     return y_test   
+
+# def pred_f2(x_test):
+#     n = alpha_2.size()[0]
+#     m = x_test.size()[0]
+#     kernel = hw2_utils.rbf(1)
+#     y_test = torch.from_numpy(np.zeros([m,]))
+#     for j in range(m):
+#         K = torch.zeros((n, 1))
+#         for i in range(n):
+#             K[i] = kernel(x_test[j], x[i])
+#             y_test[j] = (alpha_2.view((n, 1)) * y.view((n, 1)) * K).sum()
+#     return y_test   
+
+# def pred_f3(x_test):
+#     n = alpha_3.size()[0]
+#     m = x_test.size()[0]
+#     kernel = hw2_utils.rbf(2)
+#     y_test = torch.from_numpy(np.zeros([m,]))
+#     for j in range(m):
+#         K = torch.zeros((n, 1))
+#         for i in range(n):
+#             K[i] = kernel(x_test[j], x[i])
+#             y_test[j] = (alpha_3.view((n, 1)) * y.view((n, 1)) * K).sum()
+#     return y_test   
+
+# def pred_f4(x_test):
+#     n = alpha_4.size()[0]
+#     m = x_test.size()[0]
+#     kernel = hw2_utils.rbf(4)
+#     y_test = torch.from_numpy(np.zeros([m,]))
+#     for j in range(m):
+#         K = torch.zeros((n, 1))
+#         for i in range(n):
+#             K[i] = kernel(x_test[j], x[i])
+#             y_test[j] = (alpha_4.view((n, 1)) * y.view((n, 1)) * K).sum()
+#     return y_test   
+
+# hw2_utils.svm_contour(pred_f1)
+# hw2_utils.svm_contour(pred_f2)
+# hw2_utils.svm_contour(pred_f3)
+# hw2_utils.svm_contour(pred_f4)
